3c751a76e6c72c374d34fc11398dcf3fbfbfcf39
manuel-julian <mansch404@gmail.com>
2025-06-21 17:38:27 +0200

    Changed Access Code to Constant

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import { db, getDoc, doc } from './firebase.js';
 
+const accCode = "S9El7UJM8awVc1gpr6A3NRiOr30gpv3cl5xKoZLgucZ7TO9S3xb9T4oLKPfatyobuEABDPP4NzQU2VS4Zls0ojLaB45lKxPTkDPL37dDO5swMfOOhzIM1BB8ewTjdce4"
+
 async function readData(id) {
     if (!id) {
         return false;
     }
-    const ref = doc(db, 'S9El7UJM8awVc1gpr6A3NRiOr30gpv3cl5xKoZLgucZ7TO9S3xb9T4oLKPfatyobuEABDPP4NzQU2VS4Zls0ojLaB45lKxPTkDPL37dDO5swMfOOhzIM1BB8ewTjdce4', id);
+    const ref = doc(db, accCode, id);
     const snap = await getDoc(ref);
 
     if (snap.exists()) {
@@ -123,7 +125,7 @@ function readQueryValue() {
 async function loadHeaderLogo() {
     if (!companyLogoElement) return;
     try {
-        const logoRef = doc(db, 'kiyosa', 'headerimage');
+        const logoRef = doc(db, accCode, 'headerimage');
         const logoSnap = await getDoc(logoRef);
         if (logoSnap.exists()) {
             const data = logoSnap.data();
